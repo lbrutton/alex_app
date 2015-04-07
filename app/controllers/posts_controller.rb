9e96cbd4f5c4a6ab34fca62cd0057e653741b37d
Louis Brutton <louisbrutton@gmail.com>
2015-04-07 22:42:49 +0200

    fix strong parameters bug, allowing for adding of dates and start colors from post creation page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -27,7 +27,7 @@ class PostsController < ApplicationController
 	private
 
 	def post_params
-		params.require(:post).permit(:joke, :answer, :image)
+		params.require(:post).permit(:joke, :answer, :image, :date, :start_color)
 	end
 
 end
