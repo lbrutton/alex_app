6467e90dfe8b980f1253d3ce5e38b4c099cc946f
Louis Brutton <louisbrutton@gmail.com>
2016-12-13 23:19:51 +0100

    fix size for header, validations on number of characters

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ActiveRecord::Base
 	mount_uploader :image, ImageUploader # Tells rails to use this uploader for this model.
+	validates :joke, length: { maximum: 55 }
+	validates :answer, length: { maximum: 55 }
 end
